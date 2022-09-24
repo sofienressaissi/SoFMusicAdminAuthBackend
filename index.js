42aab29377c788c9bce07c65d98a56f4f520f4ca
Sofien Ressaissi <sofienressaissi@gmail.com>
2022-09-24 17:36:20 +0100

    update index.js by adding process.env.PORT || in PORT variable

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const path = require('path')
-const PORT = 8099
+const PORT = process.env.PORT || 8099
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require ('cors');
